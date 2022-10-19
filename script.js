0c0db4ea11a5bfbdd0b8a675c75696197d66a088
Jennifer Tieu <jennifer@yammm.com>
2022-10-18 22:20:44 -0500

    create files, add assignment requirements

---
+++ script.js
@@ -0,0 +1,20 @@
+(function(window, document, undefined){
+    let myLibrary = [];
+
+    // TODO: function that loops through array and displays 
+    // each book in a table or on their own "card"
+
+    // TODO: add event listener for "ADD BOOK" button
+    // TODO: add event listener to remove book
+    // TOOD: create function that toggles a book read status on Book's prototype isntance
+
+    function Book() {
+    // TOOD: the constructor...
+    // add author, title, number of pages, isRead, etc
+
+    }
+
+    function addBookToLibrary() {
+    // do stuff here
+    }
+})(window, document);
