cd675785717601b119edb96c0c8810dd80c377e8
Jennifer Tieu <jennifer@yammm.com>
2022-10-25 23:05:25 -0500

    add Book properties and method, add test books, add remove book function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 (function(window, document, undefined){
     let myLibrary = [];
 
+    let testBook1 = new Book("Joe Brown", "Brave", 200);
+    let testBook2 = new Book("Kat Tran", "Goodbye", 200);
+    
+    addBookToLibrary(testBook1);
+    addBookToLibrary(testBook2);
+
     // TODO: function that loops through array and displays 
     // each book in a table or on their own "card"
 
@@ -8,13 +14,23 @@
     // TODO: add event listener to remove book
     // TOOD: create function that toggles a book read status on Book's prototype isntance
 
-    function Book() {
-    // TOOD: the constructor...
-    // add author, title, number of pages, isRead, etc
-
+    function Book(author, title, pages) {
+        this.author = author;
+        this.title = title;
+        this.numberOfPages = pages;
+        this.isRead = false;
     }
 
-    function addBookToLibrary() {
-    // do stuff here
+    Book.prototype.read = function(){
+        this.isRead = !this.isRead;
+        console.log(this.isRead);
+    }
+
+    function addBookToLibrary(book) {
+        myLibrary.push(book);
+    }
+    
+    function removeBookFromLibrary(book) {
+        myLibrary.filter(libBook => libBook !== book);
     }
 })(window, document);
